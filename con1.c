f6766e01855d0ae0f45b644b9ee4b237d20adc94
silverware13 <thomasza@oregonstate.edu>
2018-04-10 13:48:31 -0700

    It is nice to write code.

--- con1.c
+++ con1.c
@@ -9,12 +9,15 @@
 #include <string.h>
 #include <pthread.h>
 
+void* consumer_thread();
+void* producer_thread();
+
 struct item_in_buffer {
 	int consumption_num;
 	int wait_period;
-}
+};
 
-//pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // Create a mutex lock.
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; // Create a mutex lock.
 
 int main(int argc, char **argv)
 {
@@ -23,6 +26,18 @@ int main(int argc, char **argv)
 	unsigned int ebx;
 	unsigned int ecx;
 	unsigned int edx;
+
+	//create a producer and consumer thread.	
+	printf("I am making threads\n");
+	pthread_t eat_t;
+	pthread_create( &eat_t, NULL, producer_thread, NULL); 
+
+	// Join thread.
+ 	pthread_join(eat_t, NULL);
+	 		
+	// Destroy mutex lock.	
+	pthread_mutex_destroy(&lock);
+
 
 	char vendor[13];
 	
@@ -35,10 +50,20 @@ int main(int argc, char **argv)
 	                     );
 	
 	if(ecx & 0x40000000){
-		//use rdrand
+		rand(time(0)); //use rdrand
 	} else {
 		//use mt19937
 	}
 	
 	return 0;
+}
+
+void* consumer_thread()
+{
+	printf("I am a consumer_thread");
+}
+
+void* producer_thread()
+{
+	printf("I am a producer_thread");
 }
