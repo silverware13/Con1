9719bba49f5998dfbcae10993064f2046f8b9af0
silverware13 <thomasza@oregonstate.edu>
2018-04-11 10:55:46 -0700

    More thread handling.

--- con1.c
+++ con1.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #define MAX_ITEMS 32
 
+//stuct for items that threads produce and consume
 struct item {
 	int consumption_num;
 	int wait_period;
@@ -26,7 +27,8 @@ int items_in_buffer = 0;
 void* consumer_thread();
 void* producer_thread();
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER; //create a mutex lock
+//create mutex lock
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 int main(int argc, char **argv)
 {
@@ -35,7 +37,6 @@ int main(int argc, char **argv)
 	pthread_create( &pro_t, NULL, producer_thread, NULL);
 	sleep(1); 
 	pthread_create( &con_t, NULL, consumer_thread, NULL); 
-	printf("Made producer and consumer threads.\n");
 
 	//join threads
  	pthread_join(pro_t, NULL);
@@ -70,26 +71,40 @@ int main(int argc, char **argv)
 
 void* consumer_thread()
 {
-	printf("I am a consumer_thread\n");
-	
-	//lets take some items out of the buffer.
-	while(items_in_buffer > 0) {
-		items_in_buffer--;
-		printf("%d\n",buffer[items_in_buffer].consumption_num);
-		printf("%d\n",buffer[items_in_buffer].wait_period);
+	printf("Consumer thread created.\n");
+	while(true) {
+		if(items_in_buffer > 0) {
+			//we get the mutex lock
+			pthread_mutex_lock(&lock);
+			printf("Consomer has mutex lock.\n");
+			//we take an item out of the buffer
+			items_in_buffer--;
+			//sleep(buffer[items_in_buffer].wait_period);
+			printf("%d\n",buffer[items_in_buffer].consumption_num);
+			printf("Consumer has removed an item.\n");
+			//we release the mutex lock
+			pthread_mutex_unlock(&lock);
+			printf("Consomer has released mutex lock.\n");
+		}
 	}
 }
 
 void* producer_thread()
 {
-	printf("I am a producer_thread\n");
-	
-	//lets put some items in the buffer.
-	for(int i = 0; i < 5; i++) {
+	printf("Producer thread created.\n");
+	while(true) {
 		if(items_in_buffer < MAX_ITEMS) {
+			//we get the mutex lock
+			pthread_mutex_lock(&lock);
+			printf("Producer has mutex lock.\n");
+			//we put an item into the buffer
 			buffer[items_in_buffer].consumption_num = items_in_buffer;
 			buffer[items_in_buffer].wait_period = items_in_buffer;
 			items_in_buffer++;
+			printf("Producer has created an item.\n");
+			//we release the mutex lock
+			pthread_mutex_unlock(&lock);
+			printf("Producer has released mutex lock.\n");
 		}
 	}
 }
